ec0dd7c75e3390512d2ec3d4bb6d73d45adf5994
payalsaini911 <payalsaini9112002@gmail.com>
2022-07-08 13:58:52 +0530

    blog app

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleSm">React & Node</span>
+        <span className="headerTitleLg">Blog</span>
+      </div>
+      <img
+        className="headerImg"
+        src="https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg"
+        alt=""
+      />
+    </div>
+  );
+}
